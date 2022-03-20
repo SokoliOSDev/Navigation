085ea9b9d838c71d74b6a841ba6207c21d424441
Sokol Daniil <94307667+SokoliOSDev@users.noreply.github.com>
2022-03-20 21:29:02 +0300

    Create ViewController.swift

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Сокол Даниил on 11.03.2022.
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+
+
+
+}
